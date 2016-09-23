1a86edf021e40b762cdbf883513203d87aa6e4da
Divya Goyal <goyaldivya53@users.noreply.github.com>
2016-09-23 17:26:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.send(createTemplate(article-one));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){

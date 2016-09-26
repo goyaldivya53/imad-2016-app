9b092c660d83b2796f7b88b251bd135e2e6181a9
Divya Goyal <goyaldivya53@users.noreply.github.com>
2016-09-26 10:52:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var counter=0;
 
 var button=document.getElementById("counter");
 button.onclick=function(){
-    //make a request to counter endpoint
+    //create a request to counter endpoint
     var request=new XMLHttpRequest();    
     
     //capture response
@@ -12,9 +12,18 @@ button.onclick=function(){
       if(request.readyState===XMLHttpRequest.Done)  {
           if(request.status===200){
               var counter=request.responseText;
+               var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
           }
       }
     };
+    
+    //make a request
+    
+    request.open('GET','http://goyaldivya53.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
     
     //render counter and display in span
   /*  counter=counter+1;

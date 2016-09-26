cf4fffd13cbcf6f53f8854519b4bfdabfdc5375e
Divya Goyal <goyaldivya53@users.noreply.github.com>
2016-09-26 10:48:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+//counter code
+var counter=0;
+
+var button=document.getElementById("counter");
+button.onclick=function(){
+    //make a request to counter endpoint
+    var request=new XMLHttpRequest();    
+    
+    //capture response
+    request.onreadystateChange= function(){
+      if(request.readyState===XMLHttpRequest.Done)  {
+          if(request.status===200){
+              var counter=request.responseText;
+          }
+      }
+    };
+    
+    //render counter and display in span
+  /*  counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();*/
+    
+};
